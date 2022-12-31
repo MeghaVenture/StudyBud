7bf9a0027c973aaca196996a7cf11ff71880148a
Meghan <51386873+MeghaVenture@users.noreply.github.com>
2022-12-31 22:10:48 +0100

    Create game_screen.py
    commit - game_screen.py from computer

---
+++ game_screen.py
@@ -0,0 +1,10 @@
+# game_screen.py
+
+#IMPORTS
+from kivy.uix.screenmanager import Screen
+from kivy.lang import Builder
+
+Builder.load_file('screens/game_screen.kv')
+
+class Game_Screen(Screen):
+    pass
