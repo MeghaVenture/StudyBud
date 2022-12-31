791e58c2eb264c985d4f8ae0d397c6b1f90545e6
Meghan <51386873+MeghaVenture@users.noreply.github.com>
2022-12-31 22:22:53 +0100

    Create signup_screen.py
    commit - signup_screen.py from computer

---
+++ signup_screen.py
@@ -0,0 +1,10 @@
+# signup_screen.py
+
+# IMPORTS
+from kivy.uix.screenmanager import Screen
+from kivy.lang import Builder
+
+Builder.load_file('screens/signup_screen.kv')
+
+class SignUp_Screen(Screen):
+    pass
