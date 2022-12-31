9ef41dbb2891de227fa0301ec68ff82bdc8d9914
Meghan <51386873+MeghaVenture@users.noreply.github.com>
2022-12-31 22:24:52 +0100

    Create tools_screen.py
    commit - tools_screen.py from computer

---
+++ tools_screen.py
@@ -0,0 +1,10 @@
+# tools_screen.py
+
+# IMPORTS
+from kivy.uix.screenmanager import Screen
+from kivy.lang import Builder
+
+Builder.load_file('screens/tools_screen.kv')
+
+class Tools_Screen(Screen):
+    pass
