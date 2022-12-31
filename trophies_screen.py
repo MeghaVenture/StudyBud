28cd56808c37b55ed3f0bc4239ce272339e298b3
Meghan <51386873+MeghaVenture@users.noreply.github.com>
2022-12-31 22:26:54 +0100

    Create trophies_screen.py
    commit - trophies_screen.py from computer

---
+++ trophies_screen.py
@@ -0,0 +1,10 @@
+# trophies_screen.py
+
+# IMPORTS
+from kivy.uix.screenmanager import Screen
+from kivy.lang import Builder
+
+Builder.load_file('screens/trophies_screen.kv')
+
+class Trophies_Screen(Screen):
+    pass
