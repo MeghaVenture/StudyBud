517890e1dd2dab9c04c7015cc22554d2711d10a0
Meghan <51386873+MeghaVenture@users.noreply.github.com>
2022-12-31 22:15:08 +0100

    Create home_screen.py
    commit - home_screen.py from computer

---
+++ home_screen.py
@@ -0,0 +1,10 @@
+# home_screen.py
+
+# IMPORTS
+from kivy.uix.screenmanager import Screen
+from kivy.lang import Builder
+
+Builder.load_file('screens/home_screen.kv')
+
+class Home_Screen(Screen):
+    pass
