8e1e74200fb20df3ebfe49e4a7d8d032d881d917
Meghan <51386873+MeghaVenture@users.noreply.github.com>
2022-12-31 22:03:31 +0100

    Create cal_todo_screen.py
    commit - cal_todo_screen.py from computer

---
+++ cal_todo_screen.py
@@ -0,0 +1,10 @@
+# cal_todo_screen.py
+
+#IMPORTS
+from kivy.uix.screenmanager import Screen
+from kivy.lang import Builder
+
+Builder.load_file('screens/cal_todo_screen.kv')
+
+class Cal_ToDo_Screen(Screen):
+    pass
