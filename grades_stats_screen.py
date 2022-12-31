e7e4ce4570098c3faace597717761c96a4b9434c
Meghan <51386873+MeghaVenture@users.noreply.github.com>
2022-12-31 22:13:23 +0100

    Create grades_stats_screen.py
    commit - grades_stats_screen.py from computer

---
+++ grades_stats_screen.py
@@ -0,0 +1,10 @@
+# grades_stats_screen.py
+
+# IMPORTS
+from kivy.uix.screenmanager import Screen
+from kivy.lang import Builder
+
+Builder.load_file('screens/grades_stats_screen.kv')
+
+class Grades_Stats_Screen(Screen):
+    pass
