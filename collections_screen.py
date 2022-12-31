e68a3f8eefeb8b0e46aec2d58c81d5a344a35e2b
Meghan <51386873+MeghaVenture@users.noreply.github.com>
2022-12-31 22:06:23 +0100

    Create collections_screen.py
    commit - collections_screen.py from computer

---
+++ collections_screen.py
@@ -0,0 +1,10 @@
+# collections_screen.py
+
+#IMPORTS
+from kivy.uix.screenmanager import Screen
+from kivy.lang import Builder
+
+Builder.load_file('screens/collections_screen.kv')
+
+class Collections_Screen(Screen):
+    pass
