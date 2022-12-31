22d78e1bea77c3c3061eb8a3cb0701a273617903
Meghan <51386873+MeghaVenture@users.noreply.github.com>
2022-12-31 22:19:08 +0100

    Create msg_notif_screen.py
    commit - msg_notif_screen.py

---
+++ msg_notif_screen.py
@@ -0,0 +1,10 @@
+# msg_notif_screen.py
+
+# IMPORTS
+from kivy.uix.screenmanager import Screen
+from kivy.lang import Builder
+
+Builder.load_file('screens/msg_notif_screen.kv')
+
+class Msg_Notif_Screen(Screen):
+    pass
