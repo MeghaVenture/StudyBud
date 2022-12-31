0896ea08050a109a7125cafbbcf2c94227a2ff03
Meghan <51386873+MeghaVenture@users.noreply.github.com>
2022-12-31 22:21:04 +0100

    Create setting_screen.py
    commit - setting_screen.py from computer

---
+++ setting_screen.py
@@ -0,0 +1,10 @@
+# setting_screen.py
+
+# IMPORTS
+from kivy.uix.screenmanager import Screen
+from kivy.lang import Builder
+
+Builder.load_file('screens/setting_screen.kv')
+
+class Setting_Screen(Screen):
+    pass
