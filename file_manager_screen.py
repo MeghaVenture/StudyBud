ddb04ad7a3f5d408dd5638f0d56924b4a2a50513
Meghan <51386873+MeghaVenture@users.noreply.github.com>
2022-12-31 22:08:43 +0100

    Create file_manager_screen.py
    commit - file_manager_screen.py from computer

---
+++ file_manager_screen.py
@@ -0,0 +1,10 @@
+# file_manager_screen.py
+
+#IMPORTS
+from kivy.uix.screenmanager import Screen
+from kivy.lang import Builder
+
+Builder.load_file('screens/file_manager_screen.kv')
+
+class File_Manager_Screen(Screen):
+    pass
